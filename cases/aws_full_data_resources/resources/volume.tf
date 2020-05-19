d888dfa4939bbc87e5b88743da5f65c043c78bc9
evgkovalev <evgkovalev@croc.ru>
2020-05-19 12:12:46 +0300

    volume: add resource and symlink

---
+++ cases/aws_full_data_resources/resources/volume.tf
@@ -0,0 +1,18 @@
+resource "aws_ebs_volume" "volume" {
+  availability_zone = var.az
+  size              = 1
+
+  tags = {
+    Name = "tag_vol"
+  }
+}
+
+data "aws_ebs_volume" "data_vol" {
+  depends_on = [
+    aws_ebs_volume.volume
+  ]
+  filter {
+    name   = "tag:Name"
+    values = ["tag_vol"]
+  }
+}
